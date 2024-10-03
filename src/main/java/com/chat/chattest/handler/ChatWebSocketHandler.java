c7a442a5d1069c5f846de3e4508346803645f5b3
tlfgjadyd <tlfgjadydmk0@gmail.com>
2024-10-03 21:02:48 +0900

    first commit

---
+++ src/main/java/com/chat/chattest/handler/ChatWebSocketHandler.java
@@ -0,0 +1,31 @@
+package com.chat.chattest.handler;
+
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.TextMessage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
